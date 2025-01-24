562730bbc5474d102835f2d4a2ddc17eb228e350
Hana <hana@Hanas-MacBook-Pro.local>
2025-01-24 10:01:44 -0600

    Create model and use it in tableview

--- ANF Code Test/ANFExploreCardTableViewController.swift
+++ ANF Code Test/ANFExploreCardTableViewController.swift
@@ -7,10 +7,10 @@ import UIKit
 
 class ANFExploreCardTableViewController: UITableViewController {
 
-    private var exploreData: [[AnyHashable: Any]]? {
+    private var exploreData: [ExploreItem]? {
         if let filePath = Bundle.main.path(forResource: "exploreData", ofType: "json"),
-         let fileContent = try? Data(contentsOf: URL(fileURLWithPath: filePath)),
-         let jsonDictionary = try? JSONSerialization.jsonObject(with: fileContent, options: .mutableContainers) as? [[AnyHashable: Any]] {
+           let fileContent = try? Data(contentsOf: URL(fileURLWithPath: filePath)),
+           let jsonDictionary = try? JSONDecoder().decode([ExploreItem].self, from: fileContent) {
             return jsonDictionary
         }
         return nil
@@ -23,12 +23,12 @@ class ANFExploreCardTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = self.tableView.dequeueReusableCell(withIdentifier: "exploreContentCell", for: indexPath)
         if let titleLabel = cell.viewWithTag(1) as? UILabel,
-           let titleText = exploreData?[indexPath.row]["title"] as? String {
+           let titleText = exploreData?[indexPath.row].title {
             titleLabel.text = titleText
         }
         
         if let imageView = cell.viewWithTag(2) as? UIImageView,
-           let name = exploreData?[indexPath.row]["backgroundImage"] as? String,
+           let name = exploreData?[indexPath.row].backgroundImage,
            let image = UIImage(named: name) {
             imageView.image = image
         }
